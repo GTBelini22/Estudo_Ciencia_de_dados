b412cba2b1bd410f201b514fb370138468170b1e
Gustavo <gustavo.belini@hotmail.com>
2024-03-14 18:33:09 -0300

    Adiconado a página do modelo os parametros que necessitam ser recebidos para ser feita a analise

--- Deploy/streamlit/app/app.py
+++ Deploy/streamlit/app/app.py
@@ -3,3 +3,17 @@ import pandas as pd
 import streamlit as st
 
 # Page config
+st.set_page_config(page_title='Insurance Prediction')
+
+# Titulo da página
+st.title('Insurance Prediction')
+
+
+# Parametros
+# Esses valores de máximos e minimo são referentes ao dataset que foi feita a analise 
+var_age= st.number_input(label='Idade', value=18, min_value=18, max_value=120)
+var_bmi = st.number_input(label='BMI', value=30.0)
+var_children = st.slider(label='Número filhos', min_value=0, max_value=5)
+var_smoker = st.selectbox(label='Fumante', options=['Sim', 'Não'])
+
+# modelo de dados
