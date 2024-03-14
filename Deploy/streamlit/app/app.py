b8542cc17980504e569b89c7c01c04cf14afaaaf
Gustavo <gustavo.belini@hotmail.com>
2024-03-14 18:50:49 -0300

    Finalizado a integração do modelo com o site criado, possível prever o seguro a partir dos dados do cliente

--- Deploy/streamlit/app/app.py
+++ Deploy/streamlit/app/app.py
@@ -17,3 +17,35 @@ var_children = st.slider(label='Número filhos', min_value=0, max_value=5)
 var_smoker = st.selectbox(label='Fumante', options=['Sim', 'Não'])
 
 # modelo de dados
+# Impotando o Pickle do nosso projeto
+# models\model.pkl
+caminho_arq = r'C:\Users\gusta\OneDrive\Área de Trabalho\Cursos\DNC\Projetos\Estudo Ciencia de dados\Deploy\streamlit\models\model.pkl'
+with open(caminho_arq, 'rb') as model_file:
+    model = pickle.load(model_file)
+    
+    
+def prediction():
+    df_input = pd.DataFrame([{
+        'age': var_age,
+        'bmi': var_bmi,
+        'children': var_children,
+        'smoker': var_smoker
+    }])
+    
+    # Vai ser retornado um array np
+    prediction = model.predict(df_input)[0]
+    return prediction
+
+df_input = pd.DataFrame([{
+    'age': var_age,
+    'bmi': var_bmi,
+    'children': var_children,
+    'smoker': var_smoker
+}])
+
+
+st.write('data Frame recebdido', df_input)
+
+if st.button('Predição'):
+    insurance_cost = prediction()
+    st.success(f'Custo previsto do seguro de vida: {insurance_cost}')
