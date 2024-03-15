b000dda5cff67fa9c702484a4e4915772f0fe0b1
Gustavo <gustavo.belini@hotmail.com>
2024-03-15 15:09:58 -0300

    Criado o arquivo para a pagina principal

---
+++ Deploy/streamlit/app_multipages/project_Description.py
@@ -0,0 +1,9 @@
+import pickle
+import pandas as pd
+import streamlit as st
+
+st.set_page_config(
+    page_title = "Insurace Prediction",
+    page_icon = r"C:\Users\gusta\OneDrive\Área de Trabalho\Cursos\DNC\Projetos\Estudo Ciencia de dados\Deploy\streamlit\img\stethoscope.png"
+)
+
