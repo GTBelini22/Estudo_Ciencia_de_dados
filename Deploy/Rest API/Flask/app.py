f91811dfb5abcc106ad196cd03542fe8cdbb54c3
Gustavo <gustavo.belini@hotmail.com>
2024-03-18 10:48:53 -0300

    Configurando o método de Post do modelo

--- Deploy/Rest API/Flask/app.py
+++ Deploy/Rest API/Flask/app.py
@@ -1,6 +1,6 @@
 import pickle
 import pandas as pd
-from flask import Flask
+from flask import Flask , request
 from flask_restful import Resource, Api 
 
 
@@ -10,6 +10,27 @@ app = Flask(__name__)
 
 def home():
     return 'Bem vindo ao modelo de predição de seguros médicos API'
+
+
+
+@app.route('/predict', methods=['POST'])
+
+def index():
+    data_json = request.get_json()['data']
+    
+    # Criando um dataframe
+    df = pd.DataFrame(data_json)
+    
+    # Carregando o modelo
+    with open('models/model.pkl', 'rb') as model_file:
+        model = pickle.load(model_file)
+        
+    # A saida precisa ser alterada, um dataframe da errado na hora de sair
+    # Predições
+    output = model.predict(df).tolist()
+    return output
+    
+    
 
 
 if __name__ == '__main__':
