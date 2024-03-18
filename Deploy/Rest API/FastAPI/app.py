49b6e2280dbfe49f526efc8edb4509bbdc38f08a
Gustavo <gustavo.belini@hotmail.com>
2024-03-18 11:49:52 -0300

    Construido um endpoint get

--- Deploy/Rest API/FastAPI/app.py
+++ Deploy/Rest API/FastAPI/app.py
@@ -17,5 +17,12 @@ def home():
     return "Bem vindo ao modelo de predição de seguro de saúde"
 
 
+@app.get('/predict')
+def predict(age:int, bmi:float, children:int, smoker:str = 'no'):
+    df_input = pd.DataFrame([dict(age=age, bmi=bmi, children= children, smoker=smoker)])
+    output = model.predict(df_input)[0]
+    return output
+
+
 if __name__ == '__main__':
     uvicorn.run(app)
