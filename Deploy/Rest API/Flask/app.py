004eb05cde4d5071535ff9fb235e8a47906781d8
Gustavo <gustavo.belini@hotmail.com>
2024-03-18 10:52:50 -0300

    Arrumado o erro de bad request

--- Deploy/Rest API/Flask/app.py
+++ Deploy/Rest API/Flask/app.py
@@ -1,36 +1,23 @@
 import pickle
 import pandas as pd
-from flask import Flask , request
-from flask_restful import Resource, Api 
-
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-
 def home():
-    return 'Bem vindo ao modelo de predição de seguros médicos API'
-
-
+    return "Welcome to the Medical Insurance Prediction API"
 
 @app.route('/predict', methods=['POST'])
-
 def index():
     data_json = request.get_json()['data']
-    
-    # Criando um dataframe
     df = pd.DataFrame(data_json)
-    
-    # Carregando o modelo
+
     with open('models/model.pkl', 'rb') as model_file:
         model = pickle.load(model_file)
-        
-    # A saida precisa ser alterada, um dataframe da errado na hora de sair
-    # Predições
+    
     output = model.predict(df).tolist()
     return output
-    
-    
 
 
 if __name__ == '__main__':
